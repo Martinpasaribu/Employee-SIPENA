c58731a2c1a0773d292029ff080eaac637520c54
Anugera Rejeki Abadi <martin12082000@gmail.com>
2025-09-19 02:13:35 +0700

    update 04

--- src/utils/BaseURL.ts
+++ src/utils/BaseURL.ts
@@ -1,5 +1,5 @@
 
 
 // export const BaseURL = 'http://localhost:5000/api/v1'
-export const BaseURL = 'https://server-itr.vercel.app/api/v1'
-export const BaseURL02 = 'https://server-itr.vercel.app'
+export const BaseURL = 'https://server-sipena.vercel.app/api/v1'
+export const BaseURL02 = 'https://server-sipena.vercel.app'
